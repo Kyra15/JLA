31c4b09c2a9602d76a41ac400a63f2da053f5b5f
Kyra15 <pl251201@ahschool.com>
2025-02-06 09:47:45 -0500

    created server + copied login

--- server.py
+++ server.py
@@ -2,13 +2,33 @@ import json
 from flask import Flask, jsonify, render_template, redirect, request
 import sqlite3
 
+
+# init flask api
 app = Flask(__name__)
 
+# flask routes
 @app.route('/')
 def default():
-    return "hi"
+    return redirect('/login')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/forum')
+def forum():
+    return render_template('forum.html')
+
+@app.route('/messenger')
+def messenger():
+    return render_template('messenger.html')
 
 
+# run server
 if __name__ == '__main__':
     print("running")
     app.run(debug=True, port=4200)
